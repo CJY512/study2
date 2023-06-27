6180e97f081105aa60dd920cdfef6e1f83b2124c
cjy <c0512jy@naver.com>
2023-06-28 00:53:39 +0900

    feat: 제품 api

---
+++ cafekiosk/src/main/java/study/cafekiosk/spring/api/controller/product/ProductController.java
@@ -0,0 +1,21 @@
+package study.cafekiosk.spring.api.controller.product;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import study.cafekiosk.spring.api.service.product.ProductService;
+import study.cafekiosk.spring.api.service.product.response.ProductResponse;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class ProductController {
+
+    private final ProductService productService;
+
+    @GetMapping("/api/v1/products/selling")
+    public List<ProductResponse> getSellingProducts() {
+        return productService.getSellingProducts();
+    }
+}
