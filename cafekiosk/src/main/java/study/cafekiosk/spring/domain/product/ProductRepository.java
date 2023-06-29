0f1e0f3ec53f25ab0e5736b41adfc03dc1feac08
cjy <c0512jy@naver.com>
2023-06-30 00:59:22 +0900

    feat: native query, 가장 최근 상품 번호 조회 기능 추가

--- cafekiosk/src/main/java/study/cafekiosk/spring/domain/product/ProductRepository.java
+++ cafekiosk/src/main/java/study/cafekiosk/spring/domain/product/ProductRepository.java
@@ -1,6 +1,7 @@
 package study.cafekiosk.spring.domain.product;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -14,4 +15,7 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
     List<Product> findBySellingStatusIn(List<ProductSellingStatus> productSellingStatuses);
 
     List<Product> findAllByProductNumberIn(List<String> productNumbers);
+
+    @Query(value = "select p.product_number from product p order by p.id desc limit 1", nativeQuery = true)
+    String findLatestProductNumber();
 }
