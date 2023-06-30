bd1e624ae953764b2ec802c2d7d2da7804418fd7
cjy <c0512jy@naver.com>
2023-07-01 01:10:43 +0900

    feat: 상품 business layer와 test

--- cafekiosk/src/main/java/study/cafekiosk/spring/api/service/product/ProductService.java
+++ cafekiosk/src/main/java/study/cafekiosk/spring/api/service/product/ProductService.java
@@ -2,6 +2,9 @@ package study.cafekiosk.spring.api.service.product;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import study.cafekiosk.spring.api.controller.product.dto.request.ProductCreateRequest;
+import study.cafekiosk.spring.api.service.product.request.ProductCreateServiceRequest;
 import study.cafekiosk.spring.api.service.product.response.ProductResponse;
 import study.cafekiosk.spring.domain.product.Product;
 import study.cafekiosk.spring.domain.product.ProductRepository;
@@ -12,9 +15,33 @@ import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class ProductService {
 
     private final ProductRepository productRepository;
+
+    //동시성 이슈
+    //상품 등록을 동시에 하는 경우 ->  상품 번호가 겹칠 수 있다.
+    //1. 상품 번호에 unique 제약 조건 -> 3번 정도 다시 시도할 수 있도록 만들기
+    //2. 상품 번호를 UUID로 만들기
+    @Transactional
+    public ProductResponse createProduct(ProductCreateServiceRequest request) {
+        String nextProductNumber = createNextProductNumber();
+
+        Product product = request.toEntity(nextProductNumber);
+        Product savedProduct = productRepository.save(product);
+        return ProductResponse.of(savedProduct);
+    }
+
+    private String createNextProductNumber() {
+        String latestProductNumber = productRepository.findLatestProductNumber();
+        if (latestProductNumber == null) {
+            return "001";
+        }
+        int latestProductNumberInt = Integer.parseInt(latestProductNumber);
+        int nextProductNumberInt = latestProductNumberInt + 1;
+        return String.format("%03d", nextProductNumberInt);
+    }
 
     public List<ProductResponse> getSellingProducts() {
         return productRepository.findBySellingStatusIn(ProductSellingStatus.forDisplay())
