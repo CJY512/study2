09d719f90fce6dbd1a0c5f6ffdcbff54ef34f87c
cjy <c0512jy@naver.com>
2023-07-01 01:11:07 +0900

    feat: 상품 presentation layer와 test

--- cafekiosk/src/main/java/study/cafekiosk/spring/api/controller/product/ProductController.java
+++ cafekiosk/src/main/java/study/cafekiosk/spring/api/controller/product/ProductController.java
@@ -1,8 +1,13 @@
 package study.cafekiosk.spring.api.controller.product;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import study.cafekiosk.spring.api.ApiResponse;
+import study.cafekiosk.spring.api.controller.product.dto.request.ProductCreateRequest;
 import study.cafekiosk.spring.api.service.product.ProductService;
 import study.cafekiosk.spring.api.service.product.response.ProductResponse;
 
@@ -14,8 +19,12 @@ public class ProductController {
 
     private final ProductService productService;
 
+    @PostMapping("/api/v1/products/new")
+    public ApiResponse<ProductResponse> createProduct(@Validated @RequestBody ProductCreateRequest request) {
+        return ApiResponse.ok(productService.createProduct(request.toServiceRequest()));
+    }
     @GetMapping("/api/v1/products/selling")
-    public List<ProductResponse> getSellingProducts() {
-        return productService.getSellingProducts();
+    public ApiResponse<List<ProductResponse>> getSellingProducts() {
+        return ApiResponse.ok(productService.getSellingProducts());
     }
 }
