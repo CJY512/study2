8058b88de5f475efc0ea8b172d6c5b924fb8ea0b
cjy <c0512jy@naver.com>
2023-07-22 01:25:59 +0900

    feat: Product service layer

--- cafekiosk/src/main/java/study/cafekiosk/spring/api/service/product/ProductService.java
+++ cafekiosk/src/main/java/study/cafekiosk/spring/api/service/product/ProductService.java
@@ -19,6 +19,7 @@ import java.util.stream.Collectors;
 public class ProductService {
 
     private final ProductRepository productRepository;
+    private final ProductNumberFactory productNumberFactory;
 
     //동시성 이슈
     //상품 등록을 동시에 하는 경우 ->  상품 번호가 겹칠 수 있다.
@@ -26,21 +27,11 @@ public class ProductService {
     //2. 상품 번호를 UUID로 만들기
     @Transactional
     public ProductResponse createProduct(ProductCreateServiceRequest request) {
-        String nextProductNumber = createNextProductNumber();
+        String nextProductNumber = productNumberFactory.createNextProductNumber();
 
         Product product = request.toEntity(nextProductNumber);
         Product savedProduct = productRepository.save(product);
         return ProductResponse.of(savedProduct);
-    }
-
-    private String createNextProductNumber() {
-        String latestProductNumber = productRepository.findLatestProductNumber();
-        if (latestProductNumber == null) {
-            return "001";
-        }
-        int latestProductNumberInt = Integer.parseInt(latestProductNumber);
-        int nextProductNumberInt = latestProductNumberInt + 1;
-        return String.format("%03d", nextProductNumberInt);
     }
 
     public List<ProductResponse> getSellingProducts() {
