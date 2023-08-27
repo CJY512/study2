ab5558e64f1adb2ac6ac7c54db1dc0aeaa7021f6
CJY <c0512jy@naver.com>
2023-08-27 10:35:45 +0900

    feat: aspect version 5 using @Aspect

---
+++ springadvanced/aop/src/main/java/hello/aop/order/aop/AspectV5Order.java
@@ -0,0 +1,47 @@
+package hello.aop.order.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.core.annotation.Order;
+
+/**
+ * @Order 적용
+ * 근데 클래스단위, @Aspect 단위로만 적용된다.
+ * 작은 번호일수록 먼저 실행됨.
+ */
+@Slf4j
+public class AspectV5Order {
+
+    @Aspect
+    @Order(2)
+    public static class LogAspect {
+        @Around("hello.aop.order.aop.Pointcuts.allOrder()") //포인트컷 시그니쳐를 설정
+        public Object doLog(ProceedingJoinPoint joinPoint) throws Throwable {
+            log.info("[log] {}", joinPoint.getSignature());
+            return joinPoint.proceed();
+        }
+    }
+
+    @Aspect
+    @Order(1)
+    public static class TxAspect {
+        @Around("hello.aop.order.aop.Pointcuts.orderAndService()")
+        public Object doTransaction(ProceedingJoinPoint joinPoint) throws Throwable {
+            try {
+                log.info("[트랜잭션 시작] {}", joinPoint.getSignature());
+                Object result = joinPoint.proceed();
+                log.info("[트랜잭션 커밋] {}", joinPoint.getSignature());
+                return result;
+            } catch (Exception e) {
+                log.info("[트랜잭션 롤백] {}", joinPoint.getSignature());
+                throw e;
+            } finally {
+                log.info("[리소스 릴리즈] {}", joinPoint.getSignature());
+            }
+        }
+
+    }
+
+}
